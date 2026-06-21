1ea0747aef4d09749fe443de3a06df264ee4d2e3
LuisCrispim <fcrispim2012@gmail.com>
2026-06-21 18:57:43 -0300

    ci: adiciona relatório de testes

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://outfit-store-alpha.vercel.app',
-    screenshotOnRunFailure: false, 
+    baseUrl: 'https://outfit-store-alpha.vercel.app', 
     fixturesFolder: false,
   },
 });
