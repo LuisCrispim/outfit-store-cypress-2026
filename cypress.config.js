6d82966623c1aaff5e75fc7a69497735002878b1
LuisCrispim <fcrispim2012@gmail.com>
2026-06-21 11:09:31 -0300

    retirando opção de screenshot

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'https://outfit-store-alpha.vercel.app',
+    screenshotOnRunFailure: false, 
     fixturesFolder: false,
   },
 });
