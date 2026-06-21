026e55467bbd4278b62ed744683860a7252051b7
LuisCrispim <fcrispim2012@gmail.com>
2026-06-21 19:03:50 -0300

    ci: novos ajustes para gerar relatório de testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: false,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/reports/junit/results-[hash].xml',
+    toConsole: true,
+  },
+
   e2e: {
-    baseUrl: 'https://outfit-store-alpha.vercel.app', 
+    baseUrl: 'https://outfit-store-alpha.vercel.app',
     fixturesFolder: false,
   },
 });
